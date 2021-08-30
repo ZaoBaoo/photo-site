3d202d7e647958ed507885ad7f9f877ba72534b4
ZaoBaoo <tampooox@gmail.com>
2021-08-31 02:16:28 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        arrows:true,
+        infinite:true,
+        speed:2000
+    });
+
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
