f332b34c4b1a6cf9cd4ce605ee155dcedfac33b6
ZaoBaoo <tampooox@gmail.com>
2021-09-08 23:35:40 +0600

    123
    123

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
+
+    // Burger
+
     $('.header__burger').click(function(event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-
-
 
     $('.header__menu').click(function(event) {
         $('.header__burger,.header__menu').removeClass('active');
@@ -13,8 +14,42 @@ $(document).ready(function(){
 
 
 
+    
+
+    // Aos js
+
+    AOS.init();
+
+    // You can also pass an optional settings object
+    // below listed default settings
+    AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 700, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+    });
 
 
+
+
+
+
+    // Slider
 
     $('.slider').slick({
         autoplay:true,
@@ -24,22 +59,9 @@ $(document).ready(function(){
         speed:2000
     });
 
-    // $("[data-scroll]").on("click", function(event) {
-    //     event.preventDefault();
-
-    //     let scrollEl = $(this).data("scroll")
-    //     let scrollElPos = $(scrollEl).offset().top;
-
-    //     console.log(scrollElPos);
-
-    //     $("html, body").animate({
-    //         scrollTop: scrollElPos
-    //     }, 500)
-    // });
 });
 
 lightGallery(document.getElementById('anchor-tag'), {
-    // plugins: [lgZoom, lgThumbnail]
 });
 
 
